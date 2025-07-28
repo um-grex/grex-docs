f67f159b1782b352e1ebba939d4c42a1688a00e1
Shamov <Grigory.Shamov@umanitoba.ca>
2025-07-28 09:51:18 -0500

    GAS: fixed teh scriupt to current MCR modules

--- scripts/jobs/matlab/run-matlab-mcr.sh
+++ scripts/jobs/matlab/run-matlab-mcr.sh
@@ -6,19 +6,20 @@
 #SBATCH --job-name=Matlab-mcr-job
 #SBATCH --partition=skylake
 
-# Choose the MCR directory according to the compiler version used.
-# The one below for uofm/matlab/R2017A
-
-MCR=/global/software/matlab/mcr/v93
 
 # If running on Grex, uncomment the following line to set MCR_CACHE_ROOT:
+# the mcr version must correspond to Matlab version the code was compiled with!
 
-module load mcr/mcr
+module load mcr/R2023b
+
+# Provide the MCR directory according to the compiler version used.
+# Presently $MCRROOT variable from the mcr module points to it
+
 
 echo "Running on host: `hostname`"
 echo "Current working directory is `pwd`"
 echo "Starting run at: `date`"
 
-./run_mycode.sh $MCR > mycode_${SLURM_JOBID}.out
+./run_mycode.sh $MCRROOT > mycode_${SLURM_JOBID}.out
 
 echo "Program finished with exit code $? at: `date`"
